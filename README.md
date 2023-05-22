f4dfa829608f6cd467757df1b1b80fb13b2c3d98
testrep1 <sajadimousa@gmail.com>
2023-05-22 07:26:14 +0430

    Version 1.0.0

--- README.md
+++ README.md
@@ -27,32 +27,88 @@ pip3 install -i https://test.pypi.org/simple/ rentrylab
 
 
 ## Usage
-**To Get File:**
+**To Create URL:**
 
-To get a  code file from [rentry.co](https://pypi.org/project/rentry/), just type these two lines of code at the beginning of your file("Receive.py") and run it, your file will be updated.
+To create a new URL, just run the following command using RentryLab:
+[Please follow the rules of rentry.co, do not spam and avoid creating unnecessary URLs.](https://rentry.co/what#rules)
 ```
 import os ; from rentrylab import RentryLab
-RentryLab.myrentry().GetFromRentry("https://rentry.co/NewPage011",os.path.join(os.path.dirname(__file__)),"Receive","python",".py")
+RentryLab.myrentry().CreateRentry("https://rentry.co/NewPage011",'password',os.path.join(os.path.dirname(__file__)),StartText='Built with RentryLab'):
 ```
 
-**To Send File:**
+**To Send Code:**
 
-To send a code file to [rentry.co](https://pypi.org/project/rentry/), just add these two lines to the end of your file("Send.py"), and run it, your file will be uploaded.
+To send a code file to [rentry.co](https://pypi.org/project/rentry/), just add these two lines to the end of your file(`"Send.py"`), and run it, your file will be uploaded.
 Be careful, RentryLab will not upload your last 3 lines containing the following commands:
 ```
 import os ; from rentrylab import RentryLab
 RentryLab.myrentry().SendToRentry("https://rentry.co/NewPage011",'password',os.path.join(os.path.dirname(__file__)),'Send','python','.py',['#YourHeader' , '**Python Language**'])
 ```
 
-**To Create File:**
+**To Get Codes:**
 
-It will be added in the next version.
+To get codes file from [rentry.co](https://pypi.org/project/rentry/), just type these two lines of code at the beginning of your file(`"Receive.py"`) and run it, your file will be updated.
+This method extracts all parts of a URL containing Python codes.
+```
+import os ; from rentrylab import RentryLab
+RentryLab.myrentry().GetFromRentry("https://rentry.co/NewPage011",os.path.join(os.path.dirname(__file__)),"Receive","python",".py")
+```
 
+**To Send Data:**
 
-**Do you want to see how to use it visually???**
+To send dictionary data, proceed as follows:
+First, create an url page for data (Here `Data1`).
+Then, create a dictionary or create it with your own algorithm (Here `Dict`).
+Finally, send it to the generated URL.
+```
+import os ; from rentrylab import RentryLab
+RentryLab.myrentry().CreateRentry("https://rentry.co/Data1",'password',os.path.join(os.path.dirname(__file__)),StartText='To Test Data'):
+Dict = {'Headers' : ['DEPTH','BS','CALI','GR','DT','RT','PHIE'],
+        'r0' : [1829.2572,12,22.0052,46.6003,117.0969,0.879,0.052045],
+        'r1' : [1829.4096,12,21.9816,46.2312,'Cell','Cell','Cell'],
+        'r2' : [1829.4096,12,21.9816,46.2312,'Cell','Cell','OK']}
+RentryLab.myrentry().SendDataToRentry('https://rentry.co/Data1','password',os.path.join(os.path.dirname(__file__)),Dict,HeaderList=['# Data1','## Added with RentryLab'])
+```
 
-[windows](https://youtu.be/re4J3bZkKAI)
+**To Get Data:**
 
-[Linux](https://youtu.be/z5igWHFaPVE)
+You can get URL tables data as List of Dictionary:
+```
+import os ; from rentrylab import RentryLab
+DictList = RentryLab.myrentry().GetDataFromRentry('https://rentry.co/Data1',os.path.join(os.path.dirname(__file__)))
+# print(DictList)
+```
 
-[PythonAnyWhere](https://youtu.be/5iGLaaf8baU)
+**To Append Code:**
+
+This method adds code to the `Top` or `Down` of a URL:
+Here we added the contents of the `Append.py` file to `Down` of URL.
+```
+import os ; from rentrylab import RentryLab
+RentryLab.myrentry().AppendToRentry("https://rentry.co/NewPage011",'NewPage011',os.path.join(os.path.dirname(__file__)),'Append','python','.py','Down',['## Append','!!!note End Row'])
+```
+
+**To Append Data:**
+
+This method adds table data to the `Top` or `Down` of a URL:
+Here we added the contents of the `Dict1` to Top of URL.
+```
+import os ; from rentrylab import RentryLab
+Dict1 = {'Headers' : ['DiPTH','BS','CALI','GR','DT','QT'],
+        'r0' : [1829.2572,12,22.0052,46.6003,117.0969,0.879],
+        'r1' : [1829.4096,12,21.9816,46.2312,'Cell','Nan'],
+        'r2' : [1829.4096,12,21.9816,46.2312,'GO','NOW']}
+RentryLab.myrentry().AppendDataToRenty('https://rentry.co/Data1','Data1',os.path.join(os.path.dirname(__file__)),Dict1,'Top',HeaderList=['!!!danger 5:40 5/22/2023','## Data Append to Top'])
+```
+
+**Do you want to see how to Send or Get Codes visually?**
+
+[Send and Get Code to Windows](https://youtu.be/re4J3bZkKAI)
+
+[Send and Get Code to Linux](https://youtu.be/z5igWHFaPVE)
+
+[Send and Get Code to PythonAnyWhere](https://youtu.be/5iGLaaf8baU)
+
+**Do you want to see how works all syntax visually?**
+
+[Send and Get Code to Windows](https://youtu.be/re4J3bZkKAI)
--- rentrylab/RentryLab.py
+++ rentrylab/RentryLab.py
@@ -1,9 +1,11 @@
 import os
 import platform
-
+import json
 
 class myrentry() :
+
     def __init__(self) :
+
         self.OS = platform.system()
         if self.OS == "Windows" : # For Windows
 
@@ -12,6 +14,75 @@ class myrentry() :
         elif self.OS == "Linux" : # For Linux
 
             self.Sep = '/' # Separator
+
+
+    def DictDataMaker(self,ListString) :
+
+        Count = []
+        Delim = []
+        Size = []
+        Heada = []
+        Flag = False
+        Cook = []
+        DicCook = {}
+        for i,String in enumerate(ListString) :
+            Count.append(String.count("|"))
+            Listim = String.split("|")
+            Logical = []
+            for Elem in Listim:
+                if '-' in Elem :         
+                    Logical.append(True)
+                else :
+                    Logical.append(False)
+            Check = all(Logical)
+            if Check is True:
+                Delim.append(i)
+                Size.append(Count[-1])
+                if Count[-2] == Count[-1] :
+                    Heada.append(i-1)
+                Flag = True
+
+            if Flag == True :
+                if Count[-1] == Size[-1] :
+                    Flag = True
+                else :
+                    Flag = False
+                    DicCook["Cook{0}".format(len(Size)-1)] = Cook
+                    Cook = []
+
+            if Flag == True :
+                Cook.append(i)
+
+        # So,
+        for i,h in enumerate(Heada) :
+            DicCook["Cook"+str(i)][0] = h
+
+        # Then
+        FullData = []
+        for i,goo in enumerate(DicCook) :
+            Dicti = {}
+            for j,foo in enumerate(DicCook[goo]) :
+                if j == 0 :
+                    Dicti["Headers"] = ListString[foo].split("|")
+                else :
+                    Dicti["r"+str(j)] = ListString[foo].split("|")
+            
+            FullData.append(Dicti)
+
+        return(FullData)
+    
+
+    def CreateRentry(self,LinkName,PW,Path,StartText='This page was created with rentrylab.'):
+        file = open(Path + self.Sep + "Create.sh",'w')
+        file.write("rentry new -p " + PW + " -u https://rentry.co/" + LinkName + " " + '"' + StartText + '"')
+        file.close()
+        
+        try :
+            os.system("bash " + Path + self.Sep + "Create.sh")
+        except :
+            print("ٔError: Not Created")
+
+        os.remove(Path + self.Sep + "Create.sh") 
     
     
     def GetFromRentry(self,MyLink,Path,FileName,Lang,ext) :
@@ -26,22 +97,29 @@ class myrentry() :
         with open(Path+self.Sep+"Temp.txt") as f:
             ScriptList = f.read().splitlines()
         f.close()
-            
+
+        ScriptList = [s for s in ScriptList if s != '']
+       
         StartIndex=[idx for idx,item in enumerate(ScriptList) if item == '``` '+Lang.lower()]
         # print(StartIndex[0]+1)
         EndIndex=[idx for idx,item in enumerate(ScriptList) if item == '```']
         # print(EndIndex[0]-1)
 
-        SharedCodes = ScriptList[StartIndex[0]+1:EndIndex[0]]
+        SharedCodes = []
+        for i,Sta in enumerate(StartIndex):
+            # SharedCodes.append(["# Part "+str(i+1)])
+            SharedCodes.append(ScriptList[Sta+1:EndIndex[i]])
 
         file = open(Path + self.Sep + FileName + ext,'w')
-        for I,Text in enumerate(SharedCodes):
-            file.write(SharedCodes[I]+'\n')
+        for ie , Lista in enumerate(SharedCodes) :
+            for I,Text in enumerate(Lista):
+                file.write(Text+'\n')
+            file.write('\n')
+            file.write('\n')
         file.close()
-            
+     
         os.remove(Path + self.Sep + "Temp.txt")
-            
-        return SharedCodes
+
 
     def SendToRentry(self,MyLink,PW,Path,FileName,Lang,ext,HeaderList=['# Your Header']) :
         HeaderList.reverse()
@@ -74,6 +152,205 @@ class myrentry() :
         os.system("bash " + Path + self.Sep + "Set.sh")
 
         os.remove(Path + self.Sep + "Set.sh")    
-        os.remove(Path + self.Sep + "Temp.txt")    
+        os.remove(Path + self.Sep + "Temp.txt")
 
+
+    def GetDataFromRentry(self,MyLink,Path,*args) :
+        try :
+            file = open(Path+self.Sep+"GetData.sh",'w')
+            file.write("rentry raw -u " +  MyLink + " > " + '"' + Path + self.Sep +'Temp.txt"')
+            file.close()
+            os.system("bash " + Path + self.Sep + "GetData.sh")
+
+            os.remove(Path + self.Sep + "GetData.sh")
+
+            with open(Path+self.Sep+"Temp.txt") as f:
+                ScriptList = f.read().splitlines()
+            f.close()
+
+            os.remove(Path + self.Sep + "Temp.txt")
+
+            ScriptList = [s for s in ScriptList if s != '']
+
+            Dictlist = self.DictDataMaker(ScriptList)
+
+            if len(args) == 0 :
+                return Dictlist
+            elif len(args) != 0 and list(args)[0].lower() == 'json'  :
+                with open(Path + self.Sep + "GottenData.json", "w") as Vari:
+                    json.dump(Dictlist, Vari,indent=4)
+        
+        except Exception as e:
+            print(e)
+
+
+
+    def SendDataToRentry(self,MyLink,PW,Path,Dict,HeaderList=['# Data1']):
+        HeaderList.reverse()
+
+        ScriptList = []
+        for (i,Head) in enumerate(HeaderList) :
+            ScriptList.insert(0, Head)
+        
+
+        for i,Elem in enumerate(Dict) :
+
+            Row = list(Dict.keys())[i]
+            String = '|'.join(map(str, Dict[Row]))
+            ScriptList.append(String)
+            if i == 0 : # Header
+                Delim = '{}'.format('|'.join('-' for _ in range(len(Dict[Row]))))
+                ScriptList.append(Delim)
+            else :
+                pass
+        ScriptList.append('\n'+'***')
+
+
+        file = open(Path + self.Sep + "Temp.txt",'w')
+        for I,Text in enumerate(ScriptList):
+            file.write(Text+'\n')
+        file.close()
+
+        file = open(Path+self.Sep+"SetData.sh",'w')
+        if self.OS == "Windows" :
+            Path_ = Path.replace('\\','/')
+            file.write("cat " + Path_ + "/" + "Temp.txt" + " | rentry edit -p " + PW + " -u " + MyLink)
+        elif self.OS == 'Linux' :
+            file.write("cat " + Path + self.Sep  + "Temp.txt" + " | rentry edit -p " + PW + " -u " + MyLink)
+        file.close()
+
+        os.system("bash " + Path + self.Sep + "SetData.sh")
+
+        os.remove(Path + self.Sep + "SetData.sh")    
+        os.remove(Path + self.Sep + "Temp.txt")
+
+  
+    def AppendToRentry(self,MyLink,PW,Path,FileName,Lang,ext,Loc,HeaderList=['# Append With RentryLab']):
+        
+        file = open(Path+self.Sep+"GetToAppend.sh",'w')
+        file.write("rentry raw -u " +  MyLink + " > " + '"' + Path + self.Sep +'Temp.txt"')
+        file.close()
+        os.system("bash " + Path + self.Sep + "GetToAppend.sh")
+
+        os.remove(Path + self.Sep + "GetToAppend.sh")
+
+        with open(Path+self.Sep+"Temp.txt") as f:
+            ScriptList = f.read().splitlines()
+        f.close()
+
+        ScriptList = [s for s in ScriptList if s != '']
+
+        HeaderList.reverse()
+
+        with open(Path + self.Sep + FileName + ext) as f:
+            ScriptList1 = f.read().splitlines()
+        f.close()
+
+        ScriptList1 = ScriptList1[0:len(ScriptList1)-2]
+
+        ScriptList1.insert(0, '``` '+Lang.lower())
+        ScriptList1.append('```')
+
+        for (i,Head) in enumerate(HeaderList) :
+            ScriptList1.insert(0, Head)
+        
+        if Loc.lower() == 'down':
+            GeneList = ScriptList + ScriptList1
+        elif Loc.lower() == 'top':
+            GeneList = ScriptList1 + ScriptList
+        
+        file = open(Path + self.Sep + "Temp.txt",'w')
+        for I,Text in enumerate(GeneList):
+            file.write(Text+'\n')
+        file.close()
+
+        file = open(Path+self.Sep+"SetToAppend.sh",'w')
+        if self.OS == "Windows" :
+            Path_ = Path.replace('\\','/')
+            file.write("cat " + Path_ + "/" + "Temp.txt" + " | rentry edit -p " + PW + " -u " + MyLink)
+        elif self.OS == 'Linux' :
+            file.write("cat " + Path + self.Sep  + "Temp.txt" + " | rentry edit -p " + PW + " -u " + MyLink)
+        file.close()
+
+        os.system("bash " + Path + self.Sep + "SetToAppend.sh")
+
+        os.remove(Path + self.Sep + "SetToAppend.sh")    
+        os.remove(Path + self.Sep + "Temp.txt")
+
+
+
+    def AppendDataToRenty(self,MyLink,PW,Path,Dict,Loc,HeaderList=['# Data Append']):
+        try :
+            file = open(Path+self.Sep+"GetDataAppend.sh",'w')
+            file.write("rentry raw -u " +  MyLink + " > " + '"' + Path + self.Sep +'Temp.txt"')
+            file.close()
+            os.system("bash " + Path + self.Sep + "GetDataAppend.sh")
+
+            os.remove(Path + self.Sep + "GetDataAppend.sh")
+
+            with open(Path+self.Sep+"Temp.txt") as f:
+                ScriptList = f.read().splitlines()
+            f.close()
+
+            ScriptList = [s for s in ScriptList if s != '']
+
+            U = tuple(ScriptList)
+            P = list(U)
+            r = 0
+            for i,Elem in enumerate(U) :
+                if Elem == '***' :
+                    P[i+r:i+r] = ['\n']
+                    r += 1
+            
+            ScriptList = P
+
+            HeaderList.reverse()
+
+            ScriptList1 = []
+            for (i,Head) in enumerate(HeaderList) :
+                ScriptList1.insert(0, Head)
+            
+            for i,Elem in enumerate(Dict) :
+
+                Row = list(Dict.keys())[i]
+                String = '|'.join(map(str, Dict[Row]))
+                ScriptList1.append(String)
+                if i == 0 : # Header
+                    Delim = '{}'.format('|'.join('-' for _ in range(len(Dict[Row]))))
+                    ScriptList1.append(Delim)
+                else :
+                    pass
+            
+            ScriptList1.append('\n'+'***')
+            
+            if Loc.lower() == 'down':
+                GeneList = ScriptList + ScriptList1
+            elif Loc.lower() == 'top':
+                GeneList = ScriptList1 + ScriptList
+
+
+            file = open(Path + self.Sep + "Temp.txt",'w')
+            for I,Text in enumerate(GeneList):
+                file.write(Text+'\n')
+            file.write('\n')
+            file.close()
+
+            file = open(Path+self.Sep+"SetDataAppend.sh",'w')
+            if self.OS == "Windows" :
+                Path_ = Path.replace('\\','/')
+                file.write("cat " + Path_ + "/" + "Temp.txt" + " | rentry edit -p " + PW + " -u " + MyLink)
+            elif self.OS == 'Linux' :
+                file.write("cat " + Path + self.Sep  + "Temp.txt" + " | rentry edit -p " + PW + " -u " + MyLink)
+            file.close()
+
+            os.system("bash " + Path + self.Sep + "SetDataAppend.sh")
+
+            os.remove(Path + self.Sep + "SetDataAppend.sh")    
+            os.remove(Path + self.Sep + "Temp.txt")
+
+        except Exception as e:
+            print(e)
+
+
+################### 
 
